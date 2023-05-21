db37ad538a3a6b98383a8e1ec3494324d1e1b366
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-05-21 10:47:45 +0530

    def is_perfect_square(number):     if number<0:         return False     square_root=int(number**0.5)     return square_root * square_root == number num=int(input()) if is_perfect_square(num):     print("True") else:     print("False")

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -2,7 +2,7 @@ def is_perfect_square(number):
     if number<0:
         return False
     square_root=int(number**0.5)
-    return square_root * square_root==number
+    return square_root * square_root == number
 num=int(input())
 if is_perfect_square(num):
     print("True")
