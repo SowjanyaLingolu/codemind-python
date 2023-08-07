056bb32b601afb5818ad1fd1e17c8ce4fa175b2e
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-07 19:46:35 +0530

    n=int(input()) s=[] x=0 while(n):     r=n%10     if r in s:         print("Not Unique Number")         x=1         break     else:         s.append(r)     n=n//10 if x==0:     print("Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,6 +1,15 @@
-n=input()
-n1=set(n)
-if len(n1)==len(n):
+n=int(input())
+s=[]
+x=0
+while(n):
+    r=n%10
+    if r in s:
+        print("Not Unique Number")
+        x=1
+        break
+    else:
+        s.append(r)
+    n=n//10
+if x==0:
     print("Unique Number")
-else:
-    print("Not Unique Number")
+    
