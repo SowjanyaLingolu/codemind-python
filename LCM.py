60b88f3b3ed47a23c5a019aadbda87badb1feeb9
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-07 19:32:01 +0530

    import math a,b=map(int,input().split()) print(int(a*b/math.gcd(a,b)))

--- LCM.py
+++ LCM.py
@@ -1,9 +1,3 @@
-def lcm(a,b):
-    for i in range(1,a+1):
-        for j in range(1,b+1):
-            if a%i==0 and b%i==0:
-                g=i
-    I=(a*b)/g
-    print(int(I))
+import math
 a,b=map(int,input().split())
-lcm(a,b)
+print(int(a*b/math.gcd(a,b)))
