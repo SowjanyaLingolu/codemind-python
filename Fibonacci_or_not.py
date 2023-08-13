de3d905c3eb75fb7e37b796febd9a2ca33ab2afa
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-13 10:54:08 +0530

    import math
    def isPerfectSquare(x):
        s = int(math.sqrt(x))
        return s*s == x
    
    def isFibonacci(n):
     return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4)
    
    n= int(input())
    if isFibonacci(n):
        print(True)
    else:
        print(False)

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,11 +1,16 @@
+
 import math
-def per(x):
-    s=int(math.sqrt(x))
-    return s*s==x
-def fibo(n):
-    return per(5*n*n+4) or per(5*n*n-4)
-num=int(input())
-if(fibo(num)==True):
-    print("True")
+ 
+
+def isPerfectSquare(x):
+    s = int(math.sqrt(x))
+    return s*s == x
+
+def isFibonacci(n):
+ return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4)
+
+n= int(input())
+if isFibonacci(n):
+    print(True)
 else:
-    print("False")
+    print(False)
