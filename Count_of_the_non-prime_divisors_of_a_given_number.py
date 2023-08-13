b7dd4b48ca552f41f4ca39e125de90c8395759f5
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-13 11:25:08 +0530

    def npd(N):     count=0     for i in range(1,N+1):         if N%i==0:             if is_prime(i)==False:                 count+=1     return count def is_prime(n):     if n<=1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True N=int(input()) print(npd(N))

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,11 +1,11 @@
 def npd(N):
-    c=0
+    count=0
     for i in range(1,N+1):
         if N%i==0:
-            if prime(i)==False:
-                c+=1
-    return c
-def prime(n):
+            if is_prime(i)==False:
+                count+=1
+    return count
+def is_prime(n):
     if n<=1:
         return False
     for i in range(2,int(n**0.5)+1):
